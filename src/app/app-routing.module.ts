eb6587d2c009762873e4fc65e55718e4cc1f9025
Ryan <ryan@rmiller.dev>
2020-01-01 15:04:08 -0500

    refactor into components folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import {HubComponent} from './hub/hub.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {HubComponent} from './components/hub/hub.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HubComponent } from './hub/hub.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { HubComponent } from './components/hub/hub.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import {HttpClientModule} from '@angular/common/http';
-import { UserChatsComponent } from './hub/user-chats/user-chats.component';
-import { ChatMessagesComponent } from './hub/chat-messages/chat-messages.component';
+import { UserChatsComponent } from './components/hub/user-chats/user-chats.component';
+import { ChatMessagesComponent } from './components/hub/chat-messages/chat-messages.component';
 
 @NgModule({
   declarations: [
